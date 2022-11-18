022b52b84d741f3f7e140b6af236fec94429c91a
Rick Getz <rick@presagetech.com>
2022-11-18 15:43:19 -0500

    feat(core): Changed preprocessing format to a list instead of a dictionary to support continuous reading mode without repetitive data transforms.

--- tests/create_json.py
+++ tests/create_json.py
@@ -3,8 +3,8 @@ import sys
 sys.path.append('..')
 from presage_physiology_preprocessing import process
 
-# vid_path = "test_videos/mobile_example.mp4"
-vid_path = "/Users/rick/Desktop/test_videos/vid_aya_15x30bpm.avi"
+vid_path = "test_videos/mobile_example.mp4"
+# vid_path = "/Users/rick/Desktop/test_videos/vid_aya_15x30bpm.avi"
 trace = process(vid_path)
-with open("test_traces/float_point_example.json", "w") as f:
+with open("test_traces/mobile_example.json", "w") as f:
     json.dump(trace, f)
